fbe1f55e362241f243bc29c6d39419712713af97
pratibha-surya <suryavanshipratibha18@gmail.com>
2025-11-19 13:13:58 +0530

    fhng

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ const clientDistPath = path.join(__dirname, "../client/dist");
 app.use(express.static(clientDistPath));
 
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(clientDistPath, "index.html"));
 });
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         //target: "http://localhost:5000",
-        target:"https://login-signup-3-wa6b.onrender.com",
+        target:"https://task-for-netroot.onrender.com",
         changeOrigin: true,
         secure: false,
       }
